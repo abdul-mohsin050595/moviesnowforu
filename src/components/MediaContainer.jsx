bd63781f330ea0a6e7ab497f8e750f1faa87cc71
abdul mohsin <mohsin050595@gmail.com>
2023-11-09 16:02:46 +0530

    removed unnessary imports

--- src/components/MediaContainer.jsx
+++ src/components/MediaContainer.jsx
@@ -2,15 +2,10 @@ import Container from "react-bootstrap/Container";
 import useFetch from "../hooks/useFetch";
 import MediaItems from "./MediaItems";
 import ComponentLoading from "./Loading/ComponentLoading";
-import Error from "./Error";
 
 const MediaContainer = ({ header, mediaData, mediaType }) => {
-  // const { loading, data, error } = useFetch(`${mediaType}/${mediaCategory}`);
-
   return (
     <>
-      {/* {loading && <ComponentLoading />}
-      {error && <Error message={error} />} */}
       <Container fluid={true} className="mt-5 mb-5">
         <span className="border-info border-bottom fs-3 fw-bold border-3">
           {header}
